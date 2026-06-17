e9736bb2a6623937cabe07ec4d3373d41eca52de
jhoanjimz2 <jhoan.developer@gmail.com>
2026-06-17 18:01:02 -0500

    update og

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,17 +8,26 @@ export const metadata: Metadata = {
   authors: [{ name: 'Jhoan Jiménez' }],
   creator: 'Jhoan Jiménez',
   openGraph: {
-    type: 'website',
-    locale: 'es_CO',
-    url: 'https://jhoanjimz.dev',
-    title: 'Jhoan Jiménez — Senior Full Stack Developer',
-    description: 'Senior Full Stack Developer con más de 6 años de experiencia.',
-    siteName: 'Jhoan Jiménez Portfolio',
+  type: 'website',
+  locale: 'es_CO',
+  url: 'https://jhoan-jimenez.netlify.app',
+  title: 'Jhoan Jiménez — Senior Full Stack Developer',
+  description: 'Senior Full Stack Developer con más de 6 años de experiencia.',
+  siteName: 'Jhoan Jiménez Portfolio',
+  images: [
+      {
+        url: 'https://jhoan-jimenez.netlify.app/og-image.png',
+        width: 1200,
+        height: 630,
+        alt: 'Jhoan Jiménez Portfolio',
+      },
+    ],
   },
   twitter: {
     card: 'summary_large_image',
     title: 'Jhoan Jiménez — Senior Full Stack Developer',
     description: 'Senior Full Stack Developer con más de 6 años de experiencia.',
+    images: ['https://jhoan-jimenez.netlify.app/og-image.png'],
   },
 }
 
